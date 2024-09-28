4a60c92fd284d746c794123f9ef5af4bfefe1889
Sathya Narayanan.S <113886457+Sathyanarayanansakthi@users.noreply.github.com>
2024-09-29 05:10:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ const Navbar = () => {
         </a>
 
         <a 
-        href="https://www.google.com"
+        href="thya-narayanan-site.vercel.app"
         target="_blank"
         rel="noopener norferrer"
         aria-label="SN blog"
