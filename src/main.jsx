b746523a9f2b6d3fd6785f2d3825370b54bd4b94
Sathyanarayanansakthi <sathyanarayanansakthi@gmail.com>
2024-10-15 17:09:12 +0530

    Analysics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { inject } from '@vercel/analytics';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+
+inject();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
